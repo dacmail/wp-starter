3e0e58cd66fd22388d3c26b6ed1eeead3f4d8c3a
Daniel Aguilar <dacmail@gmail.com>
2015-10-23 13:01:30 +0200

    BS header structure

--- header.php
+++ header.php
@@ -7,7 +7,20 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<header id="header">
-		<a class="logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" /></a>
-		<?php wp_nav_menu(array('container' => 'nav', 'container_id' => 'main-menu', 'container_class' => '', 'theme_location' => 'main')); ?>
+	<header class="navbar" id="header">
+		<div class="container">
+			<div class="navbar-header">
+				<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+					<span class="sr-only"><?php _e('Toggle navigation', 'ungrynerd'); ?></span>
+					<?php _e('Menu', 'ungrynerd'); ?>
+				</button>
+				<a href="<?php echo home_url(); ?>" class="logo navbar-brand"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" /></a>
+			</div>
+			<?php wp_nav_menu(array('container' => 'nav',
+								'container_id' => 'main-menu', 
+								'container_class' => 'collapse width navbar-collapse', 
+								'menu_class' => 'nav navbar-nav',
+								'theme_location' => 'main')); ?>
+
+		</div> <!--- /.container -->
 	</header>
