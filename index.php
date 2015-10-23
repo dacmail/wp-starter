4b6a1aadc4e716d90bea4b08ba4dbe037bb84b11
Daniel Aguilar <dacmail@gmail.com>
2015-10-23 13:07:12 +0200

    Addig bs clasess

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php get_header() ?>
-<div id="container" <?php body_class('clearfix'); ?>>
-	<section id="content" class="clearfix">
-		<?php get_template_part( 'loop', 'single' ); ?>
-	</section>
-	<nav class="pagination">
-		<?php previous_posts_link( __('Anterior', 'ungrynerd')); ?>
-		<?php next_posts_link( __('Siguiente', 'ungrynerd')); ?>
-	</nav>
-	<?php get_sidebar() ?>
+<div id="container" class="container">
+	<div class="row">
+		<div class="col-sm-9">
+			<section id="content" class="clearfix">
+				<?php get_template_part( 'loop', 'single' ); ?>
+			</section>
+			<nav class="pagination">
+				<?php previous_posts_link( __('Anterior', 'ungrynerd')); ?>
+				<?php next_posts_link( __('Siguiente', 'ungrynerd')); ?>
+			</nav>
+		</div>
+		<?php get_sidebar() ?>
+	</div> <!-- /.row -->
 </div>
 <?php get_footer() ?>
--- page.php
+++ page.php
@@ -1,8 +1,12 @@
 <?php get_header() ?>
-<div id="container" class="clearfix">
-	<section id="content" class="clearfix">
-		<?php get_template_part( 'loop', 'single' ); ?>
-	</section>
-	<?php get_sidebar() ?>
+<div id="container" class="container">
+	<div class="row">
+		<div class="col-sm-9">
+			<section id="content" class="clearfix">
+				<?php get_template_part( 'loop', 'single' ); ?>
+			</section>
+		</div>
+		<?php get_sidebar() ?>
+	</div> <!-- /.row -->
 </div>
 <?php get_footer() ?>
