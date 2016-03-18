eab0ff9da409e34223e7c97aec5643e707c320ae
Daniel Aguilar <dacmail@gmail.com>
2016-03-18 17:48:53 +0100

    Update meta-box framework

--- meta-box/inc/fields/checkbox-list.php
+++ meta-box/inc/fields/checkbox-list.php
@@ -1,105 +1,63 @@
 <?php
-// Prevent loading this file directly
-defined( 'ABSPATH' ) || exit;
-
-if ( ! class_exists( 'RWMB_Checkbox_List_Field' ) )
+/**
+ * Checkbox list field class.
+ */
+class RWMB_Checkbox_List_Field extends RWMB_Multiple_Values_Field
 {
-	class RWMB_Checkbox_List_Field extends RWMB_Field
+	/**
+	 * Get field HTML
+	 *
+	 * @param mixed $meta
+	 * @param array $field
+	 *
+	 * @return string
+	 */
+	static function html( $meta, $field )
 	{
-		/**
-		 * Get field HTML
-		 *
-		 * @param mixed $meta
-		 * @param array $field
-		 *
-		 * @return string
-		 */
-		static function html( $meta, $field )
+		$meta = (array) $meta;
+		$html = array();
+		$tpl  = '<label><input %s %s> %s</label>';
+
+		foreach ( $field['options'] as $value => $label )
 		{
-			$meta = (array) $meta;
-			$html = array();
-			$tpl  = '<label><input type="checkbox" class="rwmb-checkbox-list" name="%s" value="%s"%s> %s</label>';
-
-			foreach ( $field['options'] as $value => $label )
-			{
-				$html[] = sprintf(
-					$tpl,
-					$field['field_name'],
-					$value,
-					checked( in_array( $value, $meta ), 1, false ),
-					$label
-				);
-			}
-
-			return implode( '<br>', $html );
+			$attributes = self::get_attributes( $field, $value );
+			$html[]     = sprintf(
+				$tpl,
+				self::render_attributes( $attributes ),
+				checked( in_array( (string) $value, $meta, true ), 1, false ),
+				$label
+			);
 		}
 
-		/**
-		 * Get meta value
-		 * If field is cloneable, value is saved as a single entry in DB
-		 * Otherwise value is saved as multiple entries (for backward compatibility)
-		 *
-		 * @see "save" method for better understanding
-		 *
-		 * TODO: A good way to ALWAYS save values in single entry in DB, while maintaining backward compatibility
-		 *
-		 * @param $post_id
-		 * @param $saved
-		 * @param $field
-		 *
-		 * @return array
-		 */
-		static function meta( $post_id, $saved, $field )
-		{
-			$meta = get_post_meta( $post_id, $field['id'], $field['clone'] );
-			$meta = ( ! $saved && '' === $meta || array() === $meta ) ? $field['std'] : $meta;
-			$meta = array_map( 'esc_attr', (array) $meta );
+		return implode( '<br>', $html );
+	}
 
-			return $meta;
-		}
+	/**
+	 * Normalize parameters for field
+	 * @param array $field
+	 * @return array
+	 */
+	static function normalize( $field )
+	{
+		$field = parent::normalize( $field );
+		$field = RWMB_Checkbox_Field::normalize( $field );
 
-		/**
-		 * Save meta value
-		 * If field is cloneable, value is saved as a single entry in DB
-		 * Otherwise value is saved as multiple entries (for backward compatibility)
-		 *
-		 * TODO: A good way to ALWAYS save values in single entry in DB, while maintaining backward compatibility
-		 *
-		 * @param $new
-		 * @param $old
-		 * @param $post_id
-		 * @param $field
-		 */
-		static function save( $new, $old, $post_id, $field )
-		{
-			if ( ! $field['clone'] )
-			{
-				parent::save( $new, $old, $post_id, $field );
+		return $field;
+	}
 
-				return;
-			}
+	/**
+	 * Get the attributes for field
+	 *
+	 * @param array $field
+	 * @param mixed $value
+	 *
+	 * @return array
+	 */
+	static function get_attributes( $field, $value = null )
+	{
+		$attributes       = RWMB_Checkbox_Field::get_attributes( $field, $value );
+		$attributes['id'] = false;
 
-			if ( empty( $new ) )
-				delete_post_meta( $post_id, $field['id'] );
-			else
-				update_post_meta( $post_id, $field['id'], $new );
-		}
-
-		/**
-		 * Normalize parameters for field
-		 *
-		 * @param array $field
-		 *
-		 * @return array
-		 */
-		static function normalize_field( $field )
-		{
-			$field['multiple']   = true;
-			$field['field_name'] = $field['id'];
-			if ( ! $field['clone'] )
-				$field['field_name'] .= '[]';
-
-			return $field;
-		}
+		return $attributes;
 	}
 }
--- meta-box/inc/fields/select.php
+++ meta-box/inc/fields/select.php
@@ -1,152 +1,193 @@
 <?php
-// Prevent loading this file directly
-defined( 'ABSPATH' ) || exit;
-
-if ( ! class_exists( 'RWMB_Select_Field' ) )
+/**
+ * Select field class.
+ */
+class RWMB_Select_Field extends RWMB_Field
 {
-	class RWMB_Select_Field extends RWMB_Field
+	/**
+	 * Enqueue scripts and styles
+	 */
+	static function admin_enqueue_scripts()
 	{
-		/**
-		 * Enqueue scripts and styles
-		 *
-		 * @return void
-		 */
-		static function admin_enqueue_scripts()
-		{
-			wp_enqueue_style( 'rwmb-select', RWMB_CSS_URL . 'select.css', array(), RWMB_VER );
-		}
+		wp_enqueue_style( 'rwmb-select', RWMB_CSS_URL . 'select.css', array(), RWMB_VER );
+		wp_enqueue_script( 'rwmb-select', RWMB_JS_URL . 'select.js', array(), RWMB_VER, true );
+	}
 
-		/**
-		 * Get field HTML
-		 *
-		 * @param mixed $meta
-		 * @param array $field
-		 *
-		 * @return string
-		 */
-		static function html( $meta, $field )
+	/**
+	 * Get field HTML
+	 *
+	 * @param mixed $meta
+	 * @param array $field
+	 *
+	 * @return string
+	 */
+	static function html( $meta, $field )
+	{
+		$attributes  = call_user_func( array( RW_Meta_Box::get_class_name( $field ), 'get_attributes' ), $field, $meta );
+		$html = sprintf(
+			'<select %s>',
+			self::render_attributes( $attributes )
+		);
+
+		$html .= $field['placeholder'] ? "<option value=''>{$field['placeholder']}</option>" : '<option></option>';
+
+		$html .= self::options_html( $field, $meta );
+
+		$html .= '</select>';
+
+		$html .= self::get_select_all_html( $field['multiple'] );
+
+		return $html;
+	}
+
+	/**
+	 * Normalize parameters for field
+	 *
+	 * @param array $field
+	 *
+	 * @return array
+	 */
+	static function normalize( $field )
+	{
+		$field = parent::normalize( $field );
+		$field = wp_parse_args( $field, array(
+			'size' => $field['multiple'] ? 5 : 0,
+		) );
+
+		$field['field_name'] .= ! $field['clone'] && $field['multiple'] ? '[]' : '';
+		return $field;
+	}
+
+	/**
+	 * Get the attributes for a field
+	 *
+	 * @param array $field
+	 * @param mixed $value
+	 *
+	 * @return array
+	 */
+	static function get_attributes( $field, $value = null )
+	{
+		$attributes = parent::get_attributes( $field, $value );
+		$attributes = wp_parse_args( $attributes, array(
+			'multiple' => $field['multiple'],
+			'size'     => $field['size'],
+		) );
+
+		return $attributes;
+	}
+
+	/**
+	 * Creates html for options
+	 *
+	 * @param array $field
+	 * @param mixed $meta
+	 *
+	 * @return array
+	 */
+	static function options_html( $field, $meta )
+	{
+		$html = '';
+
+		$option = '<option value="%s"%s>%s</option>';
+
+		foreach ( $field['options'] as $value => $label )
 		{
-			$html = sprintf(
-				'<select class="rwmb-select" name="%s" id="%s" size="%s"%s>',
-				$field['field_name'],
-				$field['id'],
-				$field['size'],
-				$field['multiple'] ? ' multiple="multiple"' : ''
+			$html .= sprintf(
+				$option,
+				$value,
+				selected( in_array( (string) $value, (array) $meta, true ), true, false ),
+				$label
 			);
-
-			$html .= self::options_html( $field, $meta );
-
-			$html .= '</select>';
-
-			return $html;
 		}
 
-		/**
-		 * Get meta value
-		 * If field is cloneable, value is saved as a single entry in DB
-		 * Otherwise value is saved as multiple entries (for backward compatibility)
-		 *
-		 * @see "save" method for better understanding
-		 *
-		 * TODO: A good way to ALWAYS save values in single entry in DB, while maintaining backward compatibility
-		 *
-		 * @param $post_id
-		 * @param $saved
-		 * @param $field
-		 *
-		 * @return array
-		 */
-		static function meta( $post_id, $saved, $field )
+		return $html;
+	}
+
+	/**
+	 * Output the field value
+	 * Display unordered list of option labels, not option values
+	 *
+	 * @param  array    $field   Field parameters
+	 * @param  array    $args    Additional arguments. Not used for these fields.
+	 * @param  int|null $post_id Post ID. null for current post. Optional.
+	 *
+	 * @return string Link(s) to post
+	 */
+	static function the_value( $field, $args = array(), $post_id = null )
+	{
+		$value = self::get_value( $field, $args, $post_id );
+		if ( ! $value )
+			return '';
+
+		$function = array( RW_Meta_Box::get_class_name( $field ), 'get_option_label' );
+
+		if ( $field['clone'] )
 		{
-			$single = $field['clone'] || ! $field['multiple'];
-			$meta   = get_post_meta( $post_id, $field['id'], $single );
-			$meta   = ( ! $saved && '' === $meta || array() === $meta ) ? $field['std'] : $meta;
-
-			$meta = array_map( 'esc_attr', (array) $meta );
-
-			return $meta;
-		}
-
-		/**
-		 * Save meta value
-		 * If field is cloneable, value is saved as a single entry in DB
-		 * Otherwise value is saved as multiple entries (for backward compatibility)
-		 *
-		 * TODO: A good way to ALWAYS save values in single entry in DB, while maintaining backward compatibility
-		 *
-		 * @param $new
-		 * @param $old
-		 * @param $post_id
-		 * @param $field
-		 */
-		static function save( $new, $old, $post_id, $field )
-		{
-			if ( ! $field['clone'] )
+			$output = '<ul>';
+			if ( $field['multiple'] )
 			{
-				parent::save( $new, $old, $post_id, $field );
-
-				return;
+				foreach ( $value as $subvalue )
+				{
+					$output .= '<li>';
+					array_walk_recursive( $subvalue, $function, $field );
+					$output .= '<ul><li>' . implode( '</li><li>', $subvalue ) . '</li></ul>';
+					$output .= '</li>';
+				}
 			}
-
-			if ( empty( $new ) )
-				delete_post_meta( $post_id, $field['id'] );
 			else
-				update_post_meta( $post_id, $field['id'], $new );
-		}
-
-		/**
-		 * Normalize parameters for field
-		 *
-		 * @param array $field
-		 *
-		 * @return array
-		 */
-		static function normalize_field( $field )
-		{
-			$field = wp_parse_args( $field, array(
-				'desc'        => '',
-				'name'        => $field['id'],
-				'size'        => $field['multiple'] ? 5 : 0,
-				'placeholder' => '',
-			) );
-			if ( ! $field['clone'] && $field['multiple'] )
-				$field['field_name'] .= '[]';
-
-			return $field;
-		}
-
-		/**
-		 * Creates html for options
-		 *
-		 * @param array $field
-		 * @param mixed $meta
-		 *
-		 * @return array
-		 */
-		static function options_html( $field, $meta )
-		{
-			$html = '';
-			if ( $field['placeholder'] )
 			{
-				$show_placeholder = ( 'select' == $field['type'] ) // Normal select field
-					|| ( isset( $field['field_type'] ) && 'select' == $field['field_type'] ) // For 'post' field
-					|| ( isset( $field['display_type'] ) && 'select' == $field['display_type'] ); // For 'taxonomy' field
-				$html = $show_placeholder ? "<option value=''>{$field['placeholder']}</option>" : '<option></option>';
+				array_walk_recursive( $value, $function, $field );
+				$output = '<li>' . implode( '</li><li>', $value ) . '</li>';
 			}
-
-			$option = '<option value="%s"%s>%s</option>';
-
-			foreach ( $field['options'] as $value => $label )
-			{
-				$html .= sprintf(
-					$option,
-					$value,
-					selected( in_array( $value, (array) $meta ), true, false ),
-					$label
-				);
-			}
-
-			return $html;
+			$output .= '</ul>';
 		}
+		else
+		{
+			if ( $field['multiple'] )
+			{
+				array_walk_recursive( $value, $function, $field );
+				$output = '<ul><li>' . implode( '</li><li>', $value ) . '</li></ul>';
+			}
+			else
+			{
+				call_user_func_array( $function, array( &$value, 0, $field ) );
+				$output = $value;
+			}
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Get option label to display in the frontend
+	 *
+	 * @param int   $value Option value
+	 * @param int   $index Array index
+	 * @param array $field Field parameter
+	 *
+	 * @return string
+	 */
+	static function get_option_label( &$value, $index, $field )
+	{
+		$value = $field['options'][$value];
+	}
+
+	/**
+	 * Get html for select all|none for multiple select
+	 *
+	 * @param $multiple
+	 *
+	 * @return string
+	 */
+	static function get_select_all_html( $multiple )
+	{
+		if ( $multiple === true )
+		{
+			return '<div class="rwmb-select-all-none">
+					' . __( 'Select', 'meta-box' ) . ': <a data-type="all" href="#">' . __( 'All', 'meta-box' ) . '</a> | <a data-type="none" href="#">' . __( 'None', 'meta-box' ) . '</a>
+				</div>';
+		}
+		return '';
 	}
 }
