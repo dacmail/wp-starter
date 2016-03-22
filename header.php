7dcd2fe41544edde0f939aaeee40856572a1ef71
Daniel Aguilar <dacmail@gmail.com>
2016-03-22 09:39:09 +0100

    Enlace home escapado

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 					<span class="sr-only"><?php _e('Toggle navigation', 'ungrynerd'); ?></span>
 					<?php _e('Menu', 'ungrynerd'); ?>
 				</button>
-				<a href="<?php echo home_url(); ?>" class="logo navbar-brand"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" /></a>
+				<a href="<?php echo esc_url(home_url('/')); ?>" class="logo navbar-brand"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" /></a>
 			</div>
 			<?php wp_nav_menu(array('container' => 'nav',
 								'container_id' => 'main-menu', 
