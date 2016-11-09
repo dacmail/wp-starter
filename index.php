82c86adf1a238bcd637facc5763d867d309a398e
Daniel Aguilar <dacmail@gmail.com>
2016-11-09 11:04:24 +0100

    Pagination

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
 			<section id="content" class="clearfix">
 				<?php get_template_part( 'loop', 'single' ); ?>
 			</section>
-			<nav class="pagination">
-				<?php previous_posts_link( __('Anterior', 'ungrynerd')); ?>
-				<?php next_posts_link( __('Siguiente', 'ungrynerd')); ?>
-			</nav>
+			<div class="pagination-wrap">
+				<?php ungrynerd_pagination(); ?>
+			</div>
 		</div>
 		<?php get_sidebar() ?>
 	</div> <!-- /.row -->
