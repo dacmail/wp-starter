9012896ce485f03dc94ef29a2be23e86d9b00461
Daniel Aguilar <dacmail@gmail.com>
2017-03-30 16:27:01 +0200

    basic css structure

--- 404.php
+++ 404.php
@@ -1,8 +1,6 @@
 <?php get_header() ?>
 <section class="container">
-	<section id="content" class="clearfix">
-		<h1 class="post-title"><?php _e('La p&aacute;gina que buscas no existe', 'ungrynerd'); ?></h1>
-		<p class="post-content"><a href="<?php echo home_url(); ?>"><?php _e('Vuelve a la p&aacute;gina de inicio', 'ungrynerd'); ?></a></p>
-	</section>
+	<h1 class="post-title"><?php _e('La p&aacute;gina que buscas no existe', 'ungrynerd'); ?></h1>
+	<p class="post-content"><a href="<?php echo home_url(); ?>"><?php _e('Vuelve a la p&aacute;gina de inicio', 'ungrynerd'); ?></a></p>
 </section>
 <?php get_footer() ?>
--- single.php
+++ single.php
@@ -1,11 +1,13 @@
 <?php get_header() ?>
 <section class="container">
-    <div class="col-sm-9">
-    	<section id="content" class="clearfix">
-    		<?php get_template_part('loop', 'single'); ?>
-    		<?php comments_template(); ?>
-    	</section>
+    <div class="row">
+        <div class="col-sm-9">
+        	<section id="content" class="clearfix">
+        		<?php get_template_part('loop', 'single'); ?>
+        		<?php comments_template(); ?>
+        	</section>
+        </div>
+    	<?php get_sidebar() ?>
     </div>
-	<?php get_sidebar() ?>
 </section>
 <?php get_footer() ?>
