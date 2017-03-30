1701958924e01f157f62789519b9e6586c932ca4
Daniel Aguilar <dacmail@gmail.com>
2017-03-30 12:47:04 +0200

    Menu css

--- header.php
+++ header.php
@@ -8,14 +8,17 @@
 <body <?php body_class(); ?>>
 	<header class="header">
 		<div class="container">
-			<a href="<?php echo esc_url(home_url('/')); ?>" class="header__logo">
+			<div class="header__wrapper">
 				<?php if (has_custom_logo()): ?>
 					<?php the_custom_logo(); ?>
 				<?php else: ?>
-					<?php bloginfo('name'); ?>
+					<a href="<?php echo esc_url(home_url('/')); ?>" class="header__logo logo--text">
+						<?php bloginfo('name'); ?>
+					</a>
 				<?php endif ?>
-			</a>
-			<button class="header__menu-toggle"><?php esc_html_e('Menu', 'ungrynerd'); ?></button>
+
+				<button class="header__menu-toggle"><?php esc_html_e('Menu', 'ungrynerd'); ?></button>
+			</div>
 			<?php wp_nav_menu(array('container' => 'nav',
 								'menu_class' => 'header__menu',
 								'theme_location' => 'main',
