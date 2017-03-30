98a7de1d9abd1a602393cb503f0d0c2c7f4d5e41
Daniel Aguilar <dacmail@gmail.com>
2017-03-30 17:01:18 +0200

    Main script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,10 @@
 (function($) {
 	$(document).ready(function() {
 		//JS
+    $(document).on('click', '.header__menu-toggle', function(event) {
+      event.preventDefault();
+      $('.header__menu').toggle();
+    });
 	});
 	$(window).load(function() {
 		//JS
